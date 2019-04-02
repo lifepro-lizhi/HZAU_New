afe2bfe319d06451288677b8eecc0de3d2670ca1
powerflame2018 <powerflame426@gmail.com>
2019-04-02 21:20:03 +0800

    change STATIC_ROOT

--- HZAU_Management_System/settings.py
+++ HZAU_Management_System/settings.py
@@ -135,5 +135,7 @@ STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
