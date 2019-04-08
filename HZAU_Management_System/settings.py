9af9d6fe531caeb0b279d627d04a2ae8bdb68cee
powerflame2018 <powerflame426@gmail.com>
2019-04-08 21:52:12 +0800

    a

--- HZAU_Management_System/settings.py
+++ HZAU_Management_System/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'hzau_project_database',
+        'NAME': 'hzau_project_database1',
         'USER': 'hzau_admin',
         'PASSWORD': '12345qqq',
         'HOST': 'localhost',
