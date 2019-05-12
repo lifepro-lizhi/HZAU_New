db7f42377a8c0e9db6ccd8a5c6987e1d08019dcb
powerflame2018 <powerflame426@gmail.com>
2019-05-12 20:45:52 +0800

    version 1.0

--- HZAU_Management_System/settings.py
+++ HZAU_Management_System/settings.py
@@ -82,14 +82,14 @@ WSGI_APPLICATION = 'HZAU_Management_System.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': 'hzau_project_database',
-        # 'USER': 'hzau_admin',
-        # 'PASSWORD': '12345qqq',
-        # 'HOST': 'localhost',
-        # 'PORT': '',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hzau_project_database',
+        'USER': 'hzau_admin',
+        'PASSWORD': '12345qqq',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
