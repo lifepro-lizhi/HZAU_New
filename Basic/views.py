c2aa29d40a7f196f753e94e5ae97799318123d21
powerflame2018 <powerflame426@gmail.com>
2019-12-14 16:19:52 +0800

    modify unity url

--- Basic/views.py
+++ Basic/views.py
@@ -40,7 +40,7 @@ def guest_name(request):
     if request.method == 'POST':
         if request.POST['guest_name'] is not "":
             record_access_history(request, "游客", request.POST['guest_name'])
-            return redirect('http://211.69.130.114:8000/bird/index.html')
+            return redirect('http://211.69.130.114:9000/bird/index.html')
 
 
 def start(request):
